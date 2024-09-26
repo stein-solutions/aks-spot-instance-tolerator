da7e8b4f8edc18cde55cb0db5645ca7ec9b6e317
Marius Stein <mariusstein7@gmail.com>
2024-09-26 11:55:39 +0200

    update integration test script

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export KO_DOCKER_REPO="mariusstein77/aks-spot-instance-tolerater-2bd6b0028c6413dcf3a1c311ebc3369c"
+export KO_DOCKER_REPO="mariusstein77/aks-spot-instance-tolerater"
 
 output=$(ko build --platform=all --bare)
 image_hash=$(echo "$output" | awk -F '@' '{print $2}')
 
-export AKS_SPOT_INSTANCE_TOLERATER_ITEST_IMAGE="mariusstein77/aks-spot-instance-tolerater-2bd6b0028c6413dcf3a1c311ebc3369c@"$image_hash
+export AKS_SPOT_INSTANCE_TOLERATER_ITEST_IMAGE="mariusstein77/aks-spot-instance-tolerater@"$image_hash
 echo $AKS_SPOT_INSTANCE_TOLERATER_ITEST_IMAGE
 go test -v ./test/integration/... -count=1 -tags=integration
