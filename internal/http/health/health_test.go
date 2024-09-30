7510730f152a890fcac4e1a7be561f22590687db
Marius Stein <mariusstein7@gmail.com>
2024-09-30 10:21:42 +0200

    fixed health endpoint tests

--- internal/http/health/health_test.go
+++ internal/http/health/health_test.go
@@ -1,11 +1,14 @@
 package health
 
 import (
+	"net"
 	"net/http"
+	"strconv"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/stein-solutions/aks-spot-instance-tolerator/internal/config"
 )
 
 func TestServer(t *testing.T) {
@@ -15,16 +18,50 @@ func TestServer(t *testing.T) {
 }
 
 var _ = Describe("probes", func() {
-	Context("healthz", func() {
-		resp, err := http.Get("http://localhost:8080/healthz")
+
+	var destinationPort string
+
+	BeforeEach(func() {
+		port, err := GetFreePort()
 		Expect(err).NotTo(HaveOccurred())
-		Expect(resp.StatusCode).To(Equal(http.StatusOK))
+
+		destinationPort = strconv.Itoa(port)
+		cfg := &config.Config{
+			HealthPort: destinationPort,
+		}
+
+		go StartHealthProbes(cfg)
+	})
+
+	Context("healthz", func() {
+		It("should return status OK", func() {
+			resp, err := http.Get("http://localhost:" + destinationPort + "/healthz")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resp.StatusCode).To(Equal(http.StatusOK))
+		})
 	})
 
 	Context("readyz", func() {
-		resp, err := http.Get("http://localhost:8080/readyz")
-		Expect(err).NotTo(HaveOccurred())
-		Expect(resp.StatusCode).To(Equal(http.StatusOK))
+		It("should return status OK", func() {
+			resp, err := http.Get("http://localhost:" + destinationPort + "/readyz")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resp.StatusCode).To(Equal(http.StatusOK))
+		})
 	})
 
 })
+
+// GetFreePort asks the kernel for a free open port that is ready to use.
+func GetFreePort() (int, error) {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+	if err != nil {
+		return 0, err
+	}
+
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return 0, err
+	}
+	defer l.Close()
+	return l.Addr().(*net.TCPAddr).Port, nil
+}
